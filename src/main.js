cdbca343392861eca7335c3be498ef364b57e581
Lena_Bondar <lencha.2005@gmail.com>
2024-10-03 14:42:12 +0300

    add header html/css/js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header/header';
+// import './js/hero/hero';
+// import './js/about-me/about-me';
+// import './js/projects/projects';
+// import './js/faq/faq';
+// import './js/covers/covers';
+// import './js/reviews/reviews';
+// import './js/work-together/work-together';
+// import './js/btn-up';
